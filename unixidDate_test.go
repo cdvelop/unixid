8514e516b08e1bb14b55c5d0acf028ba5b8bdc0d
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2025-03-16 00:31:51 -0300

    Rename file from date_test.go to unixidDate_test.go

---
+++ unixidDate_test.go
@@ -0,0 +1,54 @@
+package unixid_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/cdvelop/unixid"
+)
+
+func TestIdToDate(t *testing.T) {
+	now := time.Now()
+	now_nano := now.UnixNano()
+	now_expected := now.Format("2006-01-02 15:04")
+
+	uid, err := unixid.NewHandler()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testData := map[string]struct {
+		inputData string
+		expected  string
+	}{
+		"id sin punto ok":             {fmt.Sprint(now_nano), now_expected},
+		"id 2022 ":                    {"1643317560659315800", "2022-01-27 18:06"},
+		"id 2023 ":                    {"1672949057314961800", "2023-01-05 17:04"},
+		"id 2022-01-27":               {"1643319071971938900", "2022-01-27 18:31"},
+		"id 2021-03-24":               {"1648131093739755800", "2022-03-24 11:11"},
+		"id 2022-01-19":               {"1643318806368317300", "2022-01-27 18:26"},
+		"id 2022-01-19 con .":         {"1643318806368317300.5", "2022-01-27 18:26"},
+		"id 2023-03-30 con .":         {"1680184020131482400.0", "2023-03-30 10:47"},
+		"error id 2023-03-30 con E":   {"16801E4020131482400.0", "validateID id contiene caracteres no válidos"},
+		"error id 2023-03-30 con .. ": {"16801840201.31482400.0", "validateID id contiene más de un punto"},
+		"error sin data de entrada":   {"", "validateID id contiene caracteres no válidos"},
+	}
+
+	for prueba, data := range testData {
+		t.Run((prueba + ": " + data.inputData), func(t *testing.T) {
+
+			resp, err := uid.UnixNanoToStringDate(data.inputData)
+
+			if err != nil {
+				resp = err.Error()
+			}
+
+			if resp != data.expected {
+				fmt.Println("ERROR prueba:", prueba)
+				t.Fatalf("- [%v] resultado:\n[%v]\n-expectativa:\n[%v]\n\n", data.inputData, resp, data.expected)
+			}
+		})
+	}
+
+}
