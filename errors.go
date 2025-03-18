9284faab4c316a92d0ae7a6a42244662e929de61
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2025-03-18 09:28:42 -0300

    upd error message

--- errors.go
+++ errors.go
@@ -8,6 +8,6 @@ var (
 	errConf   = errors.New("configuration (&Config = nil)")
 	errNano   = errors.New(erConfHead + "time, which returns the UnixNano() int64 method")
 	errSecond = errors.New(erConfHead + "time, which returns the UnixSecondsToDate(time_seconds int64) (date string) method")
-	erSes     = errors.New(erConfHead + "e.g.: userSessionNumber() (string, error)")
+	erSes     = errors.New(erConfHead + "e.g.: UserSessionNumber() (string, error)")
 	erNumSes  = errors.New(erConfHead + "user number does not exist to generate id")
 )
